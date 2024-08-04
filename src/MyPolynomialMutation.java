068a9c777e82e93628e2230656a1527d0d47b3b9
Carles <carles@minbari.org>
2024-08-04 13:50:52 +0200

    Initial commit

--- src/MyPolynomialMutation.java
+++ src/MyPolynomialMutation.java
@@ -1,9 +1,6 @@
-//package org.uma.jmetal.operator.mutation.impl;
 
 import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
-//import org.uma.jmetal.solution.doublesolution.DoubleSolution;
-//import org.uma.jmetal.solution.doublesolution.impl.DefaultDoubleSolution;
 import org.uma.jmetal.solution.util.repairsolution.RepairDoubleSolution;
 import org.uma.jmetal.solution.util.repairsolution.impl.RepairDoubleSolutionWithBoundValue;
 import org.uma.jmetal.util.JMetalException;
@@ -12,17 +9,11 @@ import org.uma.jmetal.util.pseudorandom.JMetalRandom;
 import org.uma.jmetal.util.pseudorandom.RandomGenerator;
 
 /**
- * This class implements a polynomial mutation operator
+ * This class implements a the new self-adaptive polynomial mutation operator
  *
- * <p>The implementation is based on the NSGA-II code available in
- * http://www.iitk.ac.in/kangal/codes.shtml
- *
- * <p>If the lower and upper bounds of a variable are the same, no mutation is carried out and the
- * bound value is returned.
- *
- * @author Antonio J. Nebro <antonio@lcc.uma.es>
- * @author Juan J. Durillo
+ * @author Jose L. Carles
  */
+ 
 @SuppressWarnings("serial")
 public class MyPolynomialMutation implements MutationOperator<MyDefaultDoubleSolution> {
   private static final double DEFAULT_PROBABILITY = 0.01;
@@ -116,72 +107,17 @@ public class MyPolynomialMutation implements MutationOperator<MyDefaultDoubleSol
     Check.isNotNull(solution);
 
     doMutation(solution);
-    /*
-    double[] x = new double[solution.getNumberOfVariables()] ;
-    double[] lb = new double[solution.getNumberOfVariables()] ;
-    double[] ub = new double[solution.getNumberOfVariables()] ;
 
-    for (int i = 0; i < x.length; i++) {
-      x[i] = solution.getVariable(i) ;
-      lb[i] = solution.getLowerBound(i) ;
-      ub[i] = solution.getUpperBound(i) ;
-    }
-
-    execute(x, lb, ub) ;
-
-    for (int i = 0; i < x.length; i++) {
-      solution.setVariable(i, x[i]);
-    }
-    */
     return solution;
   }
 
-  public double[] execute2(double[] x, double[] lowerBound, double[] upperBound) {
-    double rnd, delta1, delta2, mutPow, deltaq;
-    double y, yl, yu, val, xy;
-
-    //double distrib_index = (double) solution.getAttribute("mutpar") * (25 - 1) + 1;
-    // Antes distrib_index = distributionIndex
-    double distrib_index = 0;
-    
-    for (int i = 0; i < x.length; i++) {
-      if (randomGenerator.getRandomValue() <= mutationProbability) {
-        y = x[i];
-        yl = lowerBound[i];
-        yu = upperBound[i];
-        if (yl == yu) {
-          y = yl;
-        } else {
-          delta1 = (y - yl) / (yu - yl);
-          delta2 = (yu - y) / (yu - yl);
-          rnd = randomGenerator.getRandomValue();
-          mutPow = 1.0 / (distributionIndex + 1.0);
-          if (rnd <= 0.5) {
-            xy = 1.0 - delta1;
-            val = 2.0 * rnd + (1.0 - 2.0 * rnd) * (Math.pow(xy, distrib_index + 1.0));
-            deltaq = Math.pow(val, mutPow) - 1.0;
-          } else {
-            xy = 1.0 - delta2;
-            val = 2.0 * (1.0 - rnd) + 2.0 * (rnd - 0.5) * (Math.pow(xy, distrib_index + 1.0));
-            deltaq = 1.0 - Math.pow(val, mutPow);
-          }
-          y = y + deltaq * (yu - yl);
-          y = solutionRepair.repairSolutionVariableValue(y, yl, yu);
-        }
-        x[i] = y ;
-      }
-    }
-
-    return x ;
-  }
 
   /** Perform the mutation operation */
   private void doMutation(MyDefaultDoubleSolution solution) {
     double rnd, delta1, delta2, mutPow, deltaq;
     double y, yl, yu, val, xy;
 
-    double distrib_index = (double) solution.getAttribute("mutpar") * (25 - 1) + 1;
-    // Antes distrib_index = distributionIndex
+    double distrib_index = (double) solution.getAttribute("mutpar")
     
     for (int i = 0; i < solution.getNumberOfVariables(); i++) {
       if (randomGenerator.getRandomValue() <= mutationProbability) {
@@ -194,9 +130,8 @@ public class MyPolynomialMutation implements MutationOperator<MyDefaultDoubleSol
           delta1 = (y - yl) / (yu - yl);
           delta2 = (yu - y) / (yu - yl);
           rnd = randomGenerator.getRandomValue();
-          //mutPow = 1.0 / (distrib_index + 1.0);
+
           mutPow = 1.0 / (distributionIndex + 1.0);
-          //	mutPow = 1.0 / 20;
           
           if (rnd <= 0.5) {
             xy = 1.0 - delta1;
